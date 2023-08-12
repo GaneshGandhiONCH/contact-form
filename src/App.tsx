cd2bfc27ee2993d271be6f27a630f4ccf3396f56
Saba Phruidze <sabaphruidze01@gmail.com>
2023-08-13 00:44:31 +0400

    I added poppins bold font

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <ThemeProvider theme={themes}>
       <Header/>
       <Global/>
-      
+      fefwefwefwe
     </ThemeProvider>
   )
 }
--- src/components/Global.tsx
+++ src/components/Global.tsx
@@ -1,10 +1,15 @@
 import styled,{css,createGlobalStyle} from 'styled-components'
-
+import PoppinsBold from '../assets/fonts/Poppins-Bold.ttf';
 const Global = createGlobalStyle`
+   @font-face {
+    font-family: Poppins;
+    src: url(${PoppinsBold});
+  }
     * {
         margin: 0;
         padding:0;
         box-sizing:border-box;
+        font-family: Poppins;
     }
     body {
         background-color:${props => props.theme.background};
